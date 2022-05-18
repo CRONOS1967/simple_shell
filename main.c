5cfdeaf7bdacad775cc52805668ee1308fc24845
Cronos <kabila.desalgn@gmail.com>
2022-05-18 20:22:06 +0300

    reverting

--- main.c
+++ main.c
@@ -1,180 +1,73 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * main - the main function
+ * free_data - frees data structure
  *
- * Return: (Success) 0 always
- * ------- (Fail) we drop out the looser :)
+ * @datash: data structure
+ * Return: no return
  */
-int main(void)
+void free_data(data_shell *datash)
 {
-	sh_t data;
-	int pl;
+	unsigned int i;
 
-	_memset((void *)&data, 0, sizeof(data));
-	signal(SIGINT, signal_handler);
-	while (1)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		index_cmd(&data);
-		if (read_line(&data) < 0)
-		{
-			if (isatty(STDIN_FILENO))
-				PRINT("\n");
-			break;
-		}
-		if (split_line(&data) < 0)
-		{
-			free_data(&data);
-			continue;
-		}
-		pl = parse_line(&data);
-		if (pl == 0)
-		{
-			free_data(&data);
-			continue;
-		}
-		if (pl < 0)
-		{
-			print_error(&data);
-			continue;
-		}
-		if (process_cmd(&data) < 0)
-		{
-			print_error(&data);
-			break;
-		}
-		free_data(&data);
+		free(datash->_environ[i]);
 	}
-	free_data(&data);
-	exit(EXIT_SUCCESS);
+
+	free(datash->_environ);
+	free(datash->pid);
 }
 
 /**
- * read_line - read a line from the standard input
- * @data: a pointer to the struct of data
+ * set_data - Initialize data structure
  *
- * Return: (Success) a positive number
- * ------- (Fail) a negative number
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
  */
-int read_line(sh_t *data)
+void set_data(data_shell *datash, char **av)
 {
-	char *csr_ptr, *end_ptr, c;
-	size_t size = BUFSIZE, read_st, length, new_size;
+	unsigned int i;
 
-	data->line = malloc(size * sizeof(char));
-	if (data->line == NULL)
-		return (FAIL);
-	if (isatty(STDIN_FILENO))
-		PRINT(PROMPT);
-	for (csr_ptr = data->line, end_ptr = data->line + size;;)
-	{
-		read_st = read(STDIN_FILENO, &c, 1);
-		if (read_st == 0)
-			return (FAIL);
-		*csr_ptr++ = c;
-		if (c == '\n')
-		{
-			*csr_ptr = '\0';
-			return (SUCCESS);
-		}
-		if (csr_ptr + 2 >= end_ptr)
-		{
-			new_size = size * 2;
-			length = csr_ptr - data->line;
-			data->line = _realloc(data->line, size * sizeof(char),
-						new_size * sizeof(char));
-			if (data->line == NULL)
-				return (FAIL);
-			size = new_size;
-			end_ptr = data->line + size;
-			csr_ptr = data->line + length;
-		}
-	}
-}
-#define DELIMITER " \n\t\r\a\v"
-/**
- * split_line - splits line to tokens
- * @data: a pointer to the struct of data
- *
- * Return: (Success) a positive number
- * ------- (Fail) a negative number
- */
-int split_line(sh_t *data)
-{
-	char *token;
-	size_t size = TOKENSIZE, new_size, i = 0;
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
 
-	if (_strcmp(data->line, "\n") == 0)
-		return (FAIL);
-	data->args = malloc(size * sizeof(char *));
-	if (data->args == NULL)
-		return (FAIL);
-	token = strtok(data->line, DELIMITER);
-	if (token == NULL)
-		return (FAIL);
-	while (token)
-	{
-		data->args[i++] =  token;
-		if (i + 2 >= size)
-		{
-			new_size = size * 2;
-			data->args = _realloc(data->args, size * sizeof(char *),
-					new_size * sizeof(char *));
-			if (data->args == NULL)
-				return (FAIL);
-			size = new_size;
-		}
-		token = strtok(NULL, DELIMITER);
-	}
-	data->args[i] = NULL;
-	return (0);
-}
-#undef DELIMITER
-#define DELIMITER ":"
-/**
- * parse_line - parses arguments to valid command
- * @data: a pointer to the struct of data
- *
- * Return: (Success) a positive number
- * ------- (Fail) a negative number
- */
-int parse_line(sh_t *data)
-{
-	if (is_path_form(data) > 0)
-		return (SUCCESS);
-	if (is_builtin(data) > 0)
-	{
-		if (handle_builtin(data) < 0)
-			return (FAIL);
-		return (NEUTRAL);
-	}
-	is_short_form(data);
-	return (SUCCESS);
-}
-#undef DELIMITER
-/**
- * process_cmd - process command and execute process
- * @data: a pointer to the struct of data
- *
- * Return: (Success) a positive number
- * ------- (Fail) a negative number
- */
-int process_cmd(sh_t *data)
-{
-	pid_t pid;
-	int status;
+	for (i = 0; environ[i]; i++)
+		;
 
-	pid = fork();
-	if (pid == 0)
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
 	{
-		signal(SIGINT, SIG_DFL);
-		if (execve(data->cmd, data->args, environ) < 0)
-		data->error_msg = _strdup("not found\n");
-			return (FAIL);
+		datash->_environ[i] = _strdup(environ[i]);
 	}
-	else
-	{
-		waitpid(pid, &status, WUNTRACED);
-	}
-	return (0);
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
